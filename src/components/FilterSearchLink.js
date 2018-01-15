2d7b7ebfc1a2126995220251d3804fb120af5e97
Siarhei Vaitehovich <swarog2002@mail.ru>
2018-01-15 14:30:22 +0300

    Fix css for search field

--- src/components/FilterSearchLink.js
+++ src/components/FilterSearchLink.js
@@ -2,18 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function FilterSearchLink(props) {
-    return props.active ?
-        (
-            <div className="search-form is-active">
-                <input type="search" placeholder="Find item"/>
-                <button type="submit"><i className="material-icons">search</i></button>
-            </div>
-        ) :
-        (
-            <a onClick={props.onClick} title={props.title}>
-                <i className="material-icons">{props.icon}</i>
-            </a>
-        );
+    return (
+        <div className="search-form">
+            {
+                props.active ?
+                    (
+                        <div className="is-active">
+                            <input type="search" placeholder="Find item"/>
+                            <button type="submit"><i className="material-icons">search</i></button>
+                        </div>
+                    ) :
+                    (
+                        <a onClick={props.onClick} title={props.title}>
+                            <i className="material-icons">{props.icon}</i>
+                        </a>
+                    )
+            }
+        </div>
+    );
 }
 
 FilterSearchLink.propTypes = {
