11bf37189a14a7d7c6efa94cd18ded99cff302b7
Siarhei Vaitehovich <swarog2002@mail.ru>
2018-01-18 17:31:17 +0300

    Fix layout for search field

--- src/components/FilterSearchLink.js
+++ src/components/FilterSearchLink.js
@@ -2,26 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function FilterSearchLink(props) {
-    function renderLink() {
-        return (
-            <a onClick={props.onClick} title={props.title}>
-                <i className="material-icons">{props.icon}</i>
-            </a>
-        );
-    }
-
-    function renderSearchField() {
-        return (
-            <div className="is-active">
-                <input type="search" placeholder="Find item"/>
-                <button type="submit"><i className="material-icons">search</i></button>
-            </div>
-        );
-    }
-
     return (
-        <div className="search-form">
-            {props.active ? renderSearchField() : renderLink()}
+        <div className={`search-form${props.active ? ' is-active' : ''}`} title={props.title}>
+                <input type="search" placeholder="Find item"/>
+                <button onClick={props.onClick}><i className="material-icons">search</i></button>
         </div>
     );
 }
